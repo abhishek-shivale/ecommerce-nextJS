0ac5c05308f4577e78c80d931d457f9fdf9d3044
CommitTODev <abhishekshivale45@gmail.com>
2024-04-18 12:22:26 +0530

    next-auth done

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,19 +1,59 @@
-import NextAuth from "next-auth";
+import { prisma } from "@/lib/prisma";
+import { session } from "@/lib/session";
+import { NextAuthOptions } from "next-auth";
+import NextAuth from "next-auth/next";
 import GoogleProvider from "next-auth/providers/google";
 
+const GOOGLE_CLIENT_ID = process.env.GOOGLE_CLIENT_ID!;
+const GOOGLE_CLIENT_SECRET = process.env.GOOGLE_CLIENT_SECRET!;
 
-export const authOptions = {
+const authOption: NextAuthOptions = {
+  session: {
+    strategy: "jwt",
+  },
   providers: [
     GoogleProvider({
-      clientId: process?.env?.GOOGLE_ID || "",
-      clientSecret: process.env.GOOGLE_SECRET || "",
+      clientId: GOOGLE_CLIENT_ID,
+      clientSecret: GOOGLE_CLIENT_SECRET,
     }),
   ],
-  session: {
-    strategy: "jwt" as const,
+  callbacks: {
+    async signIn({ account, profile }) {
+      if (!profile?.email) {
+        throw new Error("No profile");
+      }
+
+      await prisma.user.upsert({
+        where: {
+          email: profile.email,
+        },
+        create: {
+          email: profile.email,
+          name: profile.name,
+        },
+        update: {
+          name: profile.name,
+        },
+      });
+      return true;
+    },
+    session,
+    async jwt({ token, user, account, profile }) {
+      if (profile) {
+        const user = await prisma.user.findUnique({
+          where: {
+            email: profile.email,
+          },
+        });
+        if (!user) {
+          throw new Error("No user found");
+        }
+        token.id = user.id;
+      }
+      return token;
+    },
   },
 };
 
-const handler = NextAuth(authOptions);
-
+const handler = NextAuth(authOption);
 export { handler as GET, handler as POST };
